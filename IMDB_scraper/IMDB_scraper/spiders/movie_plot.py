ffd5d9b63b6140be68639a80bc58b28272484fa5
Tiana0117 <yining0117@g.ucla.edu>
2021-10-25 19:13:49 -0700

    update 7:13 pm

---
+++ IMDB_scraper/IMDB_scraper/spiders/movie_plot.py
@@ -0,0 +1,25 @@
+from plotly import express as px
+from matplotlib import pyplot as plt
+from plotly.io import write_html
+
+
+# we want to extract the first 10 lines of the dataframe to prepare for the plot.
+
+
+
+df_plot = df_recommend.head(10)
+
+
+Then, we create the scatterplot with the dataframe `df_plot`. The `color` depends on the `number of shared actors`.
+
+
+python
+fig = px.scatter(df_plot,
+                 x = "movie_or_TV_name",
+                 y = "number of shared actors", 
+                 color = "number of shared actors",
+                 color_continuous_midpoint = 0,
+                 title = "Scatterplot visualizing movies with shared actors")
+    
+write_html(fig, "movie_scatter.html")
+
